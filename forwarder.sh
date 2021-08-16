c2ee5bd9c1711d0788180c23279159334bfebb32
Daniil <makhonyad@yandex.ru>
2021-08-16 12:29:08 +0200

    Block in yml file

--- forwarder.sh
+++ forwarder.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-rpm -qi splunkforwarder-8.2.1-ddff1c41e5cf.x86_64
+sudo /opt/splunkforwarder/bin/splunk status
 if [ $? -ne 0 ]
-then
-  wget https://d7wz6hmoaavd0.cloudfront.net/products/universalforwarder/releases/8.2.1/linux/splunkforwarder-8.2.1-ddff1c41e5cf-linux-2.6-x86_64.rpm
-  sudo yum install -y splunkforwarder-8.2.1-ddff1c41e5cf-linux-2.6-x86_64.rpm
-else
-  continue 
-fi
-cd /opt/splunkforwarder/bin
-sudo ./splunk start --accept-license --seed-passwd 1q2w3e4r
-sudo ./splunk enable boot-start
+then 
+    rpm -qi splunkforwarder-8.2.1-ddff1c41e5cf.x86_64
+    if [ $? -ne 0 ]
+    then
+        wget https://d7wz6hmoaavd0.cloudfront.net/products/universalforwarder/releases/8.2.1/linux/splunkforwarder-8.2.1-ddff1c41e5cf-linux-2.6-x86_64.rpm
+        sudo yum install -y splunkforwarder-8.2.1-ddff1c41e5cf-linux-2.6-x86_64.rpm
+    else
+        continue 
+    fi
+    cd /opt/splunkforwarder/bin
+    sudo ./splunk start --accept-license --seed-passwd 1q2w3e4r
+    sudo ./splunk enable boot-start
 
-sudo ./splunk add forward-server 10.66.42.160:9997 -auth admin:1q2w3e4r
-sudo ./splunk add monitor /var/log/nginx/
-sudo ./splunk add monitor /var/log/messages
-sudo ./splunk add monitor /var/log/audit/
-sudo ./splunk add monitor /var/log/secure 
+    sudo ./splunk add forward-server 10.66.42.160:9997 -auth admin:1q2w3e4r
+    sudo ./splunk add monitor /var/log/nginx/
+    sudo ./splunk add monitor /var/log/messages
+    sudo ./splunk add monitor /var/log/audit/
+    sudo ./splunk add monitor /var/log/secure 
+    echo "done" > /tmp/splunk_statux.txt
+else
+    exit 0
+fi
+exit 0
